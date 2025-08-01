b42a2958bfbb4f6ebf3911e6ffcf9882386470b6
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-08-01 11:46:37 +0530

    add todo

---
+++ TODO-1/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Todo from './components/Todo/Todo'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <Todo/>
+    </div>
+  )
+}
+
+export default App
